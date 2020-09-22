e4f95c170e0cd830221f814ec963ae5734db7753
s.waschina <s.waschina@nutrinf.uni-kiel.de>
2020-09-22 13:17:43 +0200

    New handling of media files
    - also it automatically constructs a media file that can be uploaded to
    model seed.

--- FermentationTest/extract_single_medium.R
+++ FermentationTest/extract_single_medium.R
@@ -7,6 +7,13 @@ args <- commandArgs(trailingOnly=TRUE)
 dt <- fread(args[1])
 dt <- dt[medium == args[2]]
 
-dt <- dt[,.(compunds = modelseed, name, maxFlux)]
+dt <- dt[,.(compounds = modelseed, name, maxFlux)]
+
+dt.ms <- data.table(id = dt$compounds,
+                    name = dt$name,
+                    concentration = dt$maxFlux,
+                    minflux = -1000,
+                    maxflux = dt$maxFlux)
 
 fwrite(dt, file = paste0(args[2],".csv"))
+fwrite(dt.ms, file = paste0("media/",args[2],"_for_modelseed.csv"), quote = F, sep = "\t")
